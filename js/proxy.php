f0fb7bdcc4a9c098a9834dd88812b95bbfa5e22d
Mikael Gerhardsson <mikael.gerhardsson@gmail.com>
2015-12-19 16:06:59 +0100

    proxy

--- js/proxy.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$url = 'http://api.sl.se/api2/realtimedepartures.json?key=27484b49298b4b068b6b7047f0f47a9d&siteid=3470&timewindow=30';
-	$response  = file_get_contents($url);
-
-	echo $response;
-?>
